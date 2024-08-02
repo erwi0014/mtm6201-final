361fdd2dce302ecd7185b5ec9e13b9547158447d
Joryn <erwi0014@algonquinlive.com>
2024-08-02 17:37:39 -0400

    installs slick locally and styles carousel

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 let toggleLogo = document.querySelector('.logo-size-toggler');
 let logoSize = document.querySelector('.logo-size');
+let navTitle = document.querySelector('.nav-title');
 
 toggleLogo.onclick = function( ) {
     console.log('success');
     logoSize.classList.toggle('enlarge');
+    navTitle.classList.toggle('hide');
 }
 
 $(document).ready(function(){
     $('.carousel-1').slick({
-        variableWidth: true
+        dots: true,
+        arrows:true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
   });
+  
   
