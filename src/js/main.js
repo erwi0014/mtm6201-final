d09d5f7c84f162a9f7b4440beb0f6586b0496308
Joryn <erwi0014@algonquinlive.com>
2024-08-04 16:33:52 -0400

    solves logo sizing

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,55 @@
 let toggleLogo = document.querySelector('.logo-size-toggler');
 let logoSize = document.querySelector('.logo-size');
 let navTitle = document.querySelector('.nav-title');
+let isLarge = true;
+let isScrolled = false;
 
-toggleLogo.onclick = function( ) {
-    console.log('success');
-    logoSize.classList.toggle('enlarge');
-    navTitle.classList.toggle('hide');
+function setLogoSize(large) {
+    if (large) {
+        logoSize.classList.remove('enlarge');
+        if (window.innerWidth < 992) {
+            navTitle.classList.remove('hide');
+        }
+    } else {
+        logoSize.classList.add('enlarge');
+        if (window.innerWidth < 992) {
+            navTitle.classList.add('hide');
+        }
+    }
+    isLarge = large;
 }
+
+function updateNavTitleVisibility() {
+    if (window.innerWidth >= 992) {
+        navTitle.classList.remove('hide');
+    } else if (!isLarge) {
+        navTitle.classList.add('hide');
+    }
+}
+
+toggleLogo.onclick = function() {
+    if (!isScrolled) {
+        setLogoSize(!isLarge);
+    }
+};
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        setLogoSize(false);
+        isScrolled = true;
+    } else {
+        setLogoSize(true);
+        isScrolled = false;
+    }
+});
+
+window.addEventListener('resize', updateNavTitleVisibility);
+
+// Initial setup
+updateNavTitleVisibility();
+
+
+
 
 $(document).ready(function(){
     $('.carousel-1').slick({
